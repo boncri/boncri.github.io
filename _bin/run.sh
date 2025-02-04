37f352cfc925700ebc779aeff27dce6e9d9136ed
Cristiano Boncompagni <cristiano.boncompagni@intersail.it>
2025-02-04 23:17:25 +0100

    Spostati i file da pubblicare sotto /docs

--- _bin/run.sh
+++ _bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm --name boncri-github-io -d -v $(pwd):/usr/share/nginx/html -p 8000:80 nginx
+docker run --rm --name boncri-github-io -d -v $(pwd)/docs:/usr/share/nginx/html -p 8000:80 nginx
 
 echo "Server is running on port 8000..."
